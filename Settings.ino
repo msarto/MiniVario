34feb5217513648f616418b742fe3eb611ae17de
Mihai Sarto <sarto.mihai@gmail.com>
2015-12-25 23:54:06 +0200

    Create Settings.ino

---
+++ Settings.ino
@@ -0,0 +1,22 @@
+const char BUTTON_TEST = 9;
+const char BUTTON_SETTINGS = 8;
+
+void initSettings() {
+	pinMode(BUTTON_TEST, INPUT);
+	pinMode(13, OUTPUT);
+}
+
+int buttonState = 0;
+void updateSettings() {
+	buttonState = digitalRead(BUTTON_TEST);
+	if (buttonState == LOW) {
+		buttonState = digitalRead(BUTTON_SETTINGS);
+		if (buttonState == HIGH) {
+
+		}
+	} else {
+		digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+		delay(2000);              // wait for a second
+		digitalWrite(13, LOW);
+	}
+}

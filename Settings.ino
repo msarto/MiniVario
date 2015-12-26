f5f07d881c6418d708857fab11582ad6dab75c28
Mihai Sarto <sarto.mihai@gmail.com>
2015-12-26 16:08:42 +0200

    Update Settings.ino

--- Settings.ino
+++ Settings.ino
@@ -1,13 +1,20 @@
+const boolean useSettings = false;
 const char BUTTON_TEST = 9;
 const char BUTTON_SETTINGS = 8;
 
 void initSettings() {
+	if (!useSettings) {
+		return;
+	}
 	pinMode(BUTTON_TEST, INPUT);
 	pinMode(13, OUTPUT);
 }
 
 int buttonState = 0;
 void updateSettings() {
+	if (!useSettings) {
+		return;
+	}
 	buttonState = digitalRead(BUTTON_TEST);
 	if (buttonState == LOW) {
 		buttonState = digitalRead(BUTTON_SETTINGS);

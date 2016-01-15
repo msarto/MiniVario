d0626c4357fbc3bb4154ab89f7a809d9768501d7
Mihai Sarto <sarto.mihai@gmail.com>
2016-01-15 20:32:37 +0200

    Update Vario.ino

--- Vario.ino
+++ Vario.ino
@@ -1,17 +1,19 @@
 #include <SFE_BMP180.h>
 #include <Filters.h>
+#include "Kalman.h"
 
-const boolean useLogger = true;
-const char BEEPER_PORT = 4;
+const boolean useLogger = false;
+const char BEEPER_PORT = 5;
 const char SAMPLE_RATE = 8;
-const char BEEP_INTERVAL = 5;
-const int DESC_START_FREQ = 250;
-const int CLIMB_START_FREQ = 270;
+const char BEEP_INTERVAL = 3;
+const int DESC_START_FREQ = 200;
+const int CLIMB_START_FREQ = 350;
 const int FREQ_STEP = 200;
 
 SFE_BMP180 pressure;
 const float filterSensitivity = 0.2f;
 FilterOnePole filter(LOWPASS, filterSensitivity);
+FilterOnePole filterAltitude(LOWPASS, 0.8, 0);
 
 double referrence; // the reference altitude pressure
 double temperature;
@@ -26,7 +28,6 @@ void setup() {
 	}
 	initSettings();
 	setupLCD();
-	writeLCD(0);
 	// Initialize the sensor (it is important to get calibration values stored on the device).
 	if (pressure.begin()) {
 		if (useLogger)
@@ -43,6 +44,7 @@ void setup() {
 	// Get the baseline pressure:
 	referrence = getPressure();
 	filter.setFilter(LOWPASS, filterSensitivity, referrence);
+
 	if (useLogger) {
 		Serial.print("baseline pressure: ");
 		Serial.print(referrence, 3);
@@ -50,25 +52,30 @@ void setup() {
 	}
 	// make a sound to know setup was fine initialize
 	beep(1);
+	writeLCD(11.11);
 	delay(50);
 	stopBeep();
 	delay(100);
 	beep(2);
+	writeLCD(22.22);
 	delay(50);
 	stopBeep();
 	delay(100);
 	beep(3);
+	writeLCD(33.33);
 	delay(50);
 	stopBeep();
 	delay(100);
+	writeLCD(0);
 }
 
 void loop() {
 	// Put a new pressure reading in the filter:
 	double prs = getPressure();
-	filter.input(prs);
+	double currentPresure = filter.input(prs);
 	if (noSamples == SAMPLE_RATE) {
-		double currentPresure = filter.output();
+		//double currentPresure = derivative.input(prs);//filter.output();
+		//double currentPresure = filter.output();
 		if (useLogger) {
 			Serial.print("current pressure: ");
 			Serial.print(currentPresure);
@@ -76,6 +83,7 @@ void loop() {
 		}
 		// The relative altitude from the reference point (not from the GND)
 		double relativeAltitude = pressure.altitude(currentPresure, referrence);
+		relativeAltitude = filterAltitude.input(relativeAltitude);
 		referrence = currentPresure;
 		noSamples = 0;
 		beep(relativeAltitude);
@@ -124,8 +132,10 @@ void beep(double vSpeed) {
 				Serial.println("pause beep");
 		}
 		if (abs(vSpeed) > 0.1) {
+			filterAltitude.setFrequency(1);
 			writeLCD(vSpeed);
 		} else {
+			filterAltitude.setFrequency(0.8);
 			writeLCD(0);
 		}
 
